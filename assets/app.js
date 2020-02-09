ef9959eaec5ffc1e834b0cd17a32192b13373055
linas1985 <linas.mikalcius@gmail.com>
2020-02-08 21:14:09 -0600

    Add updated content and smooth scroll functionality:

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,21 @@
 console.log("HELLO");
+
+function scrollToPanel(panel) {
+  var nav = document.getElementById('nav');
+  var scrollPosition = nav.scrollTop;
+  var distance = document.getElementById(panel).offsetTop - scrollPosition;
+  var counter = 1;
+
+  function smoothStep(n) {
+    return n * n * (3 - 2 * n);
+  }
+
+  // Smooth scroll
+  var sI = setInterval(function () {
+    counter++;
+    var position = (scrollPosition + distance) * smoothStep(counter / 30);
+    window.scrollTo(0, position);
+    if (counter >= 30)
+      clearInterval(sI);
+  }, 10);
+}
