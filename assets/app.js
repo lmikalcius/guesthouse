341cbeb7b58facce8319e598ea7f70a1645f00cf
linas1985 <linas.mikalcius@gmail.com>
2020-02-11 18:01:02 -0600

    Add working code for carousel

--- assets/app.js
+++ assets/app.js
@@ -17,3 +17,51 @@ function scrollToPanel(panel) {
       clearInterval(sI);
   }, 10);
 }
+
+var carousels = document.getElementsByClassName('carousel');
+[].forEach.call(carousels, function(carouselWrapper) {
+  // the carousel variable is within the forEach closure and referenced in the listeners
+  var carousel = carouselWrapper.getElementsByClassName("carousel__content")[0];
+  var leftButton = carouselWrapper.getElementsByClassName("panel__left-arrow")[0];
+  var rightButton = carouselWrapper.getElementsByClassName("panel__right-arrow")[0];
+  var panelCount = carousel.getElementsByClassName("panel").length;
+  var offset = 0;
+  var count = 0;
+  var maxCount = panelCount - 1;
+
+  window.addEventListener('resize', function() {
+    var carouselWidth = carousel.offsetWidth;
+    offset = carouselWidth * count;
+    carousel.style.transform = "translateX(-" + offset + "px)";
+  }, true);
+
+  leftButton.addEventListener("click", function() {
+    if (count > 0) {
+      count--;
+      var carouselWidth = carousel.offsetWidth;
+      offset = carouselWidth * count;
+      carousel.style.transform = "translateX(-" + offset + "px)";
+    }
+    if (count < maxCount) {
+      rightButton.style.display = "block";
+    }
+    if (count == 0) {
+      leftButton.style.display = "none";
+    }
+  })
+
+  rightButton.addEventListener("click", function() {
+    if (count < maxCount) {
+      var carouselWidth = carousel.offsetWidth;
+      count++;
+      offset = carouselWidth * count;
+      carousel.style.transform = "translateX(-" + offset + "px)";
+    }
+    if (count > 0) {
+      leftButton.style.display = "block";
+    }
+    if (count == maxCount) {
+      rightButton.style.display = "none";
+    }
+  })
+});
