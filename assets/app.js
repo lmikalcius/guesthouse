4943e35d4aeda5308219aaebee1534ec20759288
linas1985 <linas.mikalcius@gmail.com>
2020-03-09 20:47:07 -0500

    Begin work on vertical carousel

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+// binding action to sign up text in beta stays panel
 function scrollToSignUp() {
   var rightArrow = document.getElementById("beta-stays").getElementsByClassName("panel__right-arrow")[0];
   for (var i = 0; i < 3; i++) {
@@ -5,8 +6,8 @@ function scrollToSignUp() {
   }
 }
 
+//  HORIZONTAL CAROUSEL CODE - use forEach to create closures to hold variable state for listeners
 var carousels = document.getElementsByClassName('carousel');
-// use forEach to create closures to hold variable state for listeners
 [].forEach.call(carousels, function(carouselWrapper) {
   var carousel = carouselWrapper.getElementsByClassName("carousel__content")[0];
   var leftButton = carouselWrapper.getElementsByClassName("panel__left-arrow")[0];
@@ -114,8 +115,9 @@ var carousels = document.getElementsByClassName('carousel');
     }
   }
 });
+// END HORIZONTAL CAROUSEL CODE
 
-
+// FORM SUBMISSION CODE
 var apiKey = "9f4b18b2-03ed-4e9a-8298-3e0756ad4102";
 var successMessage = '<h1 class="panel__text" style="width: 200px;">You&#39;re all set!</h1>';
 $("#long-form").submit(function(e) {
@@ -183,69 +185,100 @@ $("#short-form-two").submit(function(e) {
     }
   });
 });
-
-var currentPanel = 0;
-var panels = $(".container");
-var recentScroll = false;
+// END FORM SUBMISSION CODE
 
 
-function scrollThere(targetElement, speed) {
-  $('html, body').stop().animate( { scrollTop: targetElement.offset().top }, speed);
-}
 
-var lastScrollTop = 0;
-$(window).scroll(function(event){
-  var st = $(this).scrollTop();
-  if (!recentScroll && st > lastScrollTop){
-    recentScroll = true;
-    window.setTimeout(() => { recentScroll = false; }, 550);
-    if (currentPanel < panels.length - 1) {
-      currentPanel++;
-      scrollThere(panels.eq(currentPanel), 500);
-    }
-  } else {
-    if (!recentScroll && currentPanel > 0) {
-      recentScroll = true;
-      window.setTimeout(() => { recentScroll = false; }, 550);
-      currentPanel--;
-      scrollThere(panels.eq(currentPanel), 500);
-    }
-  }
-  lastScrollTop = st;
-});
 
-$(".nav__item").click(function (e) {
-  e.preventDefault();
-  var ind = $('.nav__item').index(this);
-  currentPanel = ind + 1;
-  recentScroll = true;
-  window.setTimeout(() => { recentScroll = false; }, 550);
-  scrollThere(panels.eq(currentPanel), 500);
-});
+// var currentPanel = 0;
+// var panels = $(".container");
+// var recentScroll = false;
 
-$(".mobile-menu .panel__text").click(function (e) {
-  e.preventDefault();
-  var ind = $('.mobile-menu .panel__text').index(this);
-  currentPanel = ind + 1;
-  recentScroll = true;
-  window.setTimeout(() => { recentScroll = false; }, 550);
-  $(".mobile-nav__inner").toggleClass("--active");
-  $(".mobile-menu").toggleClass("--active");
-  $("body").toggleClass("--active");
-  scrollThere(panels.eq(currentPanel), 500);
-});
 
-$(".panel__arrow--down").click(function (e) {
-  e.preventDefault();
-  currentPanel = 1;
-  recentScroll = true;
-  window.setTimeout(() => { recentScroll = false; }, 550);
-  scrollThere(panels.eq(currentPanel), 500);
-});
+// function scrollThere(targetElement, speed) {
+//   $('html, body').stop().animate( { scrollTop: targetElement.offset().top }, speed);
+// }
 
+// var lastScrollTop = 0;
+// $(window).scroll(function(event){
+//   var st = $(this).scrollTop();
+//   if (!recentScroll && st > lastScrollTop){
+//     recentScroll = true;
+//     window.setTimeout(() => { recentScroll = false; }, 550);
+//     if (currentPanel < panels.length - 1) {
+//       currentPanel++;
+//       scrollThere(panels.eq(currentPanel), 500);
+//     }
+//   } else {
+//     if (!recentScroll && currentPanel > 0) {
+//       recentScroll = true;
+//       window.setTimeout(() => { recentScroll = false; }, 550);
+//       currentPanel--;
+//       scrollThere(panels.eq(currentPanel), 500);
+//     }
+//   }
+//   lastScrollTop = st;
+// });
+
+// $(".nav__item").click(function (e) {
+//   e.preventDefault();
+//   var ind = $('.nav__item').index(this);
+//   currentPanel = ind + 1;
+//   recentScroll = true;
+//   window.setTimeout(() => { recentScroll = false; }, 550);
+//   scrollThere(panels.eq(currentPanel), 500);
+// });
+
+// $(".mobile-menu .panel__text").click(function (e) {
+//   e.preventDefault();
+//   var ind = $('.mobile-menu .panel__text').index(this);
+//   currentPanel = ind + 1;
+//   recentScroll = true;
+//   window.setTimeout(() => { recentScroll = false; }, 550);
+//   $(".mobile-nav__inner").toggleClass("--active");
+//   $(".mobile-menu").toggleClass("--active");
+//   $("body").toggleClass("--active");
+//   scrollThere(panels.eq(currentPanel), 500);
+// });
+
+// $(".panel__arrow--down").click(function (e) {
+//   e.preventDefault();
+//   currentPanel = 1;
+//   recentScroll = true;
+//   window.setTimeout(() => { recentScroll = false; }, 550);
+//   scrollThere(panels.eq(currentPanel), 500);
+// });
+
+
+
+// MOBILE NAV MENU CODE
 $(".mobile-nav__inner").on('click', function(event) {
     event.preventDefault();
     $(this).toggleClass("--active");
     $(".mobile-menu").toggleClass("--active");
     $("body").toggleClass("--active");
+});
+// END MOBILE NAV MENU CODE
+
+
+var verticalCarousel = document.getElementById('vertical-carousel__content');
+var vPanelCount = $(".container").length;
+var vOffset = 0;
+var vCount = 0;
+var vMaxCount = vPanelCount - 1;
+var downButton = verticalCarousel.getElementsByClassName("panel__arrow--down")[0];
+
+// window.addEventListener('resize', function() {
+//   var carouselWidth = carousel.offsetWidth;
+//   offset = carouselWidth * count;
+//   carousel.style.transform = "translateX(-" + offset + "px)";
+// }, true);
+
+downButton.addEventListener("click", function() {
+  if (vCount == 0) {
+    vCount++;
+    var verticalCarouselHeight = verticalCarousel.offsetHeight;
+    vOffset = verticalCarouselHeight * vCount;
+    verticalCarousel.style.transform = "translateY(-" + vOffset + "px)";
+  }
 });
